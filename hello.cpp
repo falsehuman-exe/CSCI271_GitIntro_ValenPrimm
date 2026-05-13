6d0a23ef7d9a1746fdfa80634a3c61270156c235
Valen Primm <valen.ep@proton.me>
2026-05-13 10:50:39 -0700

    Updated hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
 // CSCI 27X - GitHub Upload Practice
-// Name: Valen Primm
-// Section: MCSCI-271-5132
+// Name: Valen Primm (but cooler)
+// Section: MCSCI-271-5132 (also cooler (I didn't realize I wasn't supposed to edit the name and section til now))
 
 #include <iostream>
 using namespace std;
